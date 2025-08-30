5790adafd95eceb68cfa6b5363c13b3cf3f85422
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2025-08-30 16:26:25 +0530

    Update Readme.go

--- Readme.go
+++ Readme.go
@@ -1,1 +1,14 @@
 Start go
+hello world in go 
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello Go World!!")
+}
+
+------------------------------------
+
+to run go code in terminal:- @Satyams-MacBook-Air GO Language % go run main.go 
+Hello Go World!!
